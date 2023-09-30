8dcab11e245c9b9cef29d4c062a6f10092f9cfa6
Dan Johnson <danimal.johnson@gmail.com>
2023-09-30 03:10:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Dance Drill Tools
+
+Quick-and-dirty apps to make your dance drills easier.
+
+There are no masterpieces here. To *#TrainUgly*, sometimes we must also `#programUgly()`.
+
+## Clock Drill
+
+Currently, the only tool in here. There's a button that gives you 3 random steps on a clock face. There's even a bare-bones clock face so you don't overtax your brain thinking about it...
+
---
+++ clock/clock.js
@@ -0,0 +1,30 @@
+function speakText(text) {
+  // Get the SpeechSynthesis interface
+  const synth = window.speechSynthesis;
+
+  // Create a new speech synthesis utterance object
+  const utterance = new SpeechSynthesisUtterance(text);
+
+  // Speak the utterance
+  synth.speak(utterance);
+}
+
+// Get 3 random numbers between 1 and 12
+function getThreeNumbers() {
+  let numbers = [];
+  for (let i = 0; i < 3; i++) {
+    numbers.push(Math.floor(Math.random() * 12) + 1);
+  }
+  return numbers;
+}
+
+function getNextSequence() {
+  const sequence = getThreeNumbers();
+  const sequenceText = sequence.join(", ");
+  // TODO: display the sequence on the screen
+  numtext = document.getElementById("numtext");
+  // numtext.innerHTML = sequenceText;
+  numtext.textContent = sequenceText;
+  console.log(sequenceText);
+  speakText(sequenceText);
+}
